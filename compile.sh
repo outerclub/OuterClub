81410e47b935c9a2a24f6a889280d46a10de9888
root <root@domU-12-31-39-15-09-3D.compute-1.internal>
2012-02-19 05:09:26 +0000

    changed scripts

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR='oc/server/static/css'
 java -jar tools/closure-stylesheets-20111230.jar $DIR/reset.css $DIR/footer.css $DIR/layout.css $DIR/welcome.css $DIR/trending.css $DIR/user.css $DIR/categories.css $DIR/category.css $DIR/conversation.css > oc/server/static/css/layout.compiled.css
-java -jar tools/plovr-4b3caf2b7d84.jar build config.js > oc/server/static/js/all.compiled.js
+java -jar tools/plovr-4b3caf2b7d84.jar build plovr-config.txt > oc/server/static/js/all.compiled.js
