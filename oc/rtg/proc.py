35aa0f55f519b78af5cae6779f61d28fb52b9b98
rfru <rfru@ubuntu.(none)>
2012-02-25 09:31:07 -0500

    guard rtg

--- oc/rtg/proc.py
+++ oc/rtg/proc.py
@@ -71,114 +71,119 @@ class QueueProc(threading.Thread):
     def run(self):
         while True:
             msg = self.queue.get(True)
-            if isinstance(msg,event.NewAuthKey):
-                self.auth[msg.uid] = msg.key
-            # internal messages
-            elif isinstance(msg,event.Message):
-                # distribute to path
-                if (msg.path in self.paths):
-                    print 'distribute %s' % (msg.path)
-                    p = msg.payload
-                    if msg.etype == 'happening':
-                        happening_data = {'type':p['type'],'data':p['data']}
-                        for conn in self.paths[msg.path]['conns']: 
-                            self.send(conn,[msg.etype,happening_data])
-                        self.happening.append(happening_data)
-                        # limit the stored happening nows
-                        if (len(self.happening) > QueueProc.MAX_HAPPENING):
-                            self.happening = self.happening[1:]
-                        self.save()
-                    else:
-                        for conn in self.paths[msg.path]['conns']: 
-                            p = msg.payload
-                            self.send(conn,[msg.etype,p])
-            elif msg == event.QueueKill:
-                break
-            else:
-                # process normal connection events from clients
-                conn_id = msg.conn.session.session_id
-                if isinstance(msg,event.Open):
-                    print '%s, open' % (conn_id)
-                    self.conns[conn_id] = {'conn': msg.conn}
-                elif isinstance(msg,event.Close):
-                    print '%s, close' % (conn_id)
-                    if 'uid' in self.conns[conn_id]:
-                        # remove connection from all paths
-                        for path in self.conns[conn_id]['paths']:
-                            if conn_id in self.paths[path]['conns']:
-                                before = self.paths[path]['uids']
-                                self.paths[path]['conns'].remove(conn_id)
-                                self.paths[path]['uids'] = self.connsToUids(self.paths[path]['conns'])
-                                diff = before ^ self.paths[path]['uids']
-                                if (path.startswith('/conversation/') and len(diff) > 0):
-                                    self.updateViewers(path,conn_id)
+            try:
+                if isinstance(msg,event.NewAuthKey):
+                    self.auth[msg.uid] = msg.key
+                # internal messages
+                elif isinstance(msg,event.Message):
+                    # distribute to path
+                    if (msg.path in self.paths):
+                        print 'distribute %s' % (msg.path)
+                        p = msg.payload
+                        if msg.etype == 'happening':
+                            happening_data = {'type':p['type'],'data':p['data']}
+                            for conn in self.paths[msg.path]['conns']: 
+                                self.send(conn,[msg.etype,happening_data])
+                            self.happening.append(happening_data)
+                            # limit the stored happening nows
+                            if (len(self.happening) > QueueProc.MAX_HAPPENING):
+                                self.happening = self.happening[1:]
+                            self.save()
+                        else:
+                            for conn in self.paths[msg.path]['conns']: 
+                                p = msg.payload
+                                self.send(conn,[msg.etype,p])
+                elif msg == event.QueueKill:
+                    break
+                else:
+                    # process normal connection events from clients
+                    conn_id = msg.conn.session.session_id
+                    if isinstance(msg,event.Open):
+                        print '%s, open' % (conn_id)
+                        self.conns[conn_id] = {'conn': msg.conn}
+                    elif isinstance(msg,event.Close):
+                        print '%s, close' % (conn_id)
+                        if 'uid' in self.conns[conn_id]:
+                            # remove connection from all paths
+                            for path in self.conns[conn_id]['paths']:
+                                if conn_id in self.paths[path]['conns']:
+                                    before = self.paths[path]['uids']
+                                    self.paths[path]['conns'].remove(conn_id)
+                                    self.paths[path]['uids'] = self.connsToUids(self.paths[path]['conns'])
+                                    diff = before ^ self.paths[path]['uids']
+                                    if (path.startswith('/conversation/') and len(diff) > 0):
+                                        self.updateViewers(path,conn_id)
 
-                            # no more connections? delete listening path
-                            if not self.paths[path]['conns']:
-                                del self.paths[path]
-                         
-                        # cleanup connection
-                        myConn = self.conns[conn_id]
-                        self.uid_sessions[myConn['uid']].remove(conn_id)
-                        if len(self.uid_sessions[myConn['uid']]) == 0:
-                            del self.uid_sessions[myConn['uid']]
+                                # no more connections? delete listening path
+                                if not self.paths[path]['conns']:
+                                    del self.paths[path]
+                             
+                            # cleanup connection
+                            myConn = self.conns[conn_id]
+                            self.uid_sessions[myConn['uid']].remove(conn_id)
+                            if len(self.uid_sessions[myConn['uid']]) == 0:
+                                del self.uid_sessions[myConn['uid']]
+                            self.updateOnline()
+                        del self.conns[conn_id]
+                    elif isinstance(msg,event.Auth):
+                        # success?
+                        if msg.uid in self.auth and self.auth[msg.uid] == msg.key:
+                            print 'authenticated %s %s' %(msg.uid,msg.key)
+                            self.conns[conn_id]['uid'] = msg.uid 
+                            self.conns[conn_id]['paths'] = set() 
+                            if not msg.uid in self.uid_sessions:
+                                self.uid_sessions[msg.uid] = []
+                            
+                            self.uid_sessions[msg.uid].append(conn_id)
+                            self.send(conn_id,['happening_init',self.happening])
+                            users = len(self.uid_sessions.keys())
+                            self.send(conn_id,['users',users])
+                        else:
+                            self.send(conn_id,['authRejected']);
                         self.updateOnline()
-                    del self.conns[conn_id]
-                elif isinstance(msg,event.Auth):
-                    # success?
-                    if msg.uid in self.auth and self.auth[msg.uid] == msg.key:
-                        print 'authenticated %s %s' %(msg.uid,msg.key)
-                        self.conns[conn_id]['uid'] = msg.uid 
-                        self.conns[conn_id]['paths'] = set() 
-                        if not msg.uid in self.uid_sessions:
-                            self.uid_sessions[msg.uid] = []
-                        
-                        self.uid_sessions[msg.uid].append(conn_id)
-                        self.send(conn_id,['happening_init',self.happening])
-                        users = len(self.uid_sessions.keys())
-                        self.send(conn_id,['users',users])
+                    # process events that require authentication
+                    elif conn_id in self.conns and 'uid' in self.conns[conn_id]:
+                        if isinstance(msg,event.Register):
+                            reg=[]
+                            for p in msg.paths:
+                                if not p in self.conns[conn_id]['paths']:
+                                    reg.append(['ins',p])
+                            for p in self.conns[conn_id]['paths']:
+                                if not p in msg.paths:
+                                    reg.append(['del',p])
+                                
+                            # Process path adjustments
+                            for pathAdjustment in reg:
+                                path = pathAdjustment[1]
+                                if not path in self.paths:
+                                    self.paths[path] = {'conns':set(),'uids':set()}
+
+                                before = self.paths[path]['uids']
+                                if pathAdjustment[0] == 'ins':
+                                    # add connection and path
+                                    self.conns[conn_id]['paths'].add(path)
+                                    self.paths[path]['conns'].add(conn_id)
+                                else:
+                                    # delete connection from path
+                                    self.conns[conn_id]['paths'].remove(path)
+                                    self.paths[path]['conns'].remove(conn_id)
+
+                                self.paths[path]['uids'] = self.connsToUids(self.paths[path]['conns'])
+                                
+                                if (path.startswith('/conversation/')):
+                                    # send the current list of viewers if requested
+                                    if (path in msg.paths):
+                                        self.send(conn_id, ['viewers',self.fetchViewerInfo(self.paths[path]['uids'])])
+                                    # send to others if viewers has changed
+                                    diff = before ^ self.paths[path]['uids']
+                                    if len(diff) > 0:
+                                        self.updateViewers(path,conn_id)
                     else:
                         self.send(conn_id,['authRejected']);
-                    self.updateOnline()
-                # process events that require authentication
-                elif 'uid' in self.conns[conn_id]:
-                    if isinstance(msg,event.Register):
-                        reg=[]
-                        for p in msg.paths:
-                            if not p in self.conns[conn_id]['paths']:
-                                reg.append(['ins',p])
-                        for p in self.conns[conn_id]['paths']:
-                            if not p in msg.paths:
-                                reg.append(['del',p])
-                            
-                        # Process path adjustments
-                        for pathAdjustment in reg:
-                            path = pathAdjustment[1]
-                            if not path in self.paths:
-                                self.paths[path] = {'conns':set(),'uids':set()}
-
-                            before = self.paths[path]['uids']
-                            if pathAdjustment[0] == 'ins':
-                                # add connection and path
-                                self.conns[conn_id]['paths'].add(path)
-                                self.paths[path]['conns'].add(conn_id)
-                            else:
-                                # delete connection from path
-                                self.conns[conn_id]['paths'].remove(path)
-                                self.paths[path]['conns'].remove(conn_id)
-
-                            self.paths[path]['uids'] = self.connsToUids(self.paths[path]['conns'])
-                            
-                            if (path.startswith('/conversation/')):
-                                # send the current list of viewers if requested
-                                if (path in msg.paths):
-                                    self.send(conn_id, ['viewers',self.fetchViewerInfo(self.paths[path]['uids'])])
-                                # send to others if viewers has changed
-                                diff = before ^ self.paths[path]['uids']
-                                if len(diff) > 0:
-                                    self.updateViewers(path,conn_id)
-                else:
-                    self.send(conn_id,['authRejected']);
+            except Exception as e:
+                import traceback
+                print 'unexpected error:'
+                traceback.print_exc()
                     
 
     def put(self,msg):
