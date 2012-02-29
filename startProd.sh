3cc354bb8603ff13008b57e3911e5be4e25d6243
rfru <rfru@ubuntu.(none)>
2012-02-29 15:48:22 -0500

    don't kill currently running instance

--- startProd.sh
+++ startProd.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 mkdir -p logs
 rm -f logs/*.log logs/*.err
-kill `ps aux | grep rtg.py | awk '{print $2}'` 2>/dev/null
-kill `ps aux | grep main.py | awk '{print $2}'` 2>/dev/null
 nohup python -u rtg.py > logs/rtg.log &
 nohup python -u main.py >logs/main.log &
